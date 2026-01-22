d3a9f1e61716aae9dfae1eeca6c5250bb0cd5973
Ahsan Gilman <ahsan.gilman@gmail.com>
2026-01-23 00:00:25 +0600

    Update theme, spacing, and layout

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Mobile Navigation
+    const header = document.querySelector('header');
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 20) {
+            header.classList.add('scrolled');
+        } else {
+            header.classList.remove('scrolled');
+        }
+    });
+
     const mobileMenuBtn = document.getElementById('mobile-menu');
     const navLinks = document.querySelector('.nav-links');
 
